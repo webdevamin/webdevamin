48d4499c93c182db570f816e5a0a4b479e7104a0
1chbinamin <amin.m.intichev@gmail.com>
2022-04-30 16:06:46 +0200

    update responsiveness + homepage eng

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,39 +7,59 @@ import {
     faLinkedin,
 } from '@fortawesome/free-brands-svg-icons';
 
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+    faEnvelope, faArrowRightLong
+} from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
         <footer>
             <section>
-                <Link href={'https://docs.google.com/document/d/e/2PACX-1vS9acAxKVHg0eN2LPo5EhQgrXGnL4XaucyoHbnFFiNYgQfGtczrHixNxVtLgU0vI1ILS9J6zflBfvZ4/pub'}>
-                    <a className="button button_theme" rel="noreferrer" target='_blank'>
-                        My resume
-                    </a>
-                </Link>
+                <div className="button button_theme">
+                    <Link href={'https://docs.google.com/document/d/e/2PACX-1vS9acAxKVHg0eN2LPo5EhQgrXGnL4XaucyoHbnFFiNYgQfGtczrHixNxVtLgU0vI1ILS9J6zflBfvZ4/pub'}>
+                        <a rel="noreferrer" target='_blank'>
+                            My resume
+                        </a>
+                    </Link>
+                    <FontAwesomeIcon icon={faArrowRightLong} size={'xs'} />
+                </div>
             </section>
-            <section className="social_links">
-                <Link href={'mailto:amin.m.intichev@gmail.com'}>
-                    <a rel="noreferrer" target='_blank'>
-                        <FontAwesomeIcon icon={faEnvelope} size={'2xl'} />
-                    </a>
-                </Link>
-                <Link href={'https://www.instagram.com/webdevamin/'}>
-                    <a rel="noreferrer" target='_blank'>
-                        <FontAwesomeIcon icon={faInstagram} size={'2xl'} />
-                    </a>
-                </Link>
-                <Link href={'https://www.linkedin.com/in/amin-i-1072391b0/'}>
-                    <a rel="noreferrer" target='_blank'>
-                        <FontAwesomeIcon icon={faLinkedin} size={'2xl'} />
-                    </a>
-                </Link>
-                <Link href={'https://github.com/1chbinamin'}>
-                    <a rel="noreferrer" target='_blank'>
-                        <FontAwesomeIcon icon={faGithub} size={'2xl'} />
-                    </a>
-                </Link>
+            <section className="contact">
+                <h2>Contact</h2>
+                <div className="social_links">
+                    <div>
+                        <Link href={'mailto:amin.m.intichev@gmail.com'}>
+                            <a rel="noreferrer" target='_blank'>
+                                <FontAwesomeIcon icon={faEnvelope} size={'xl'} />
+                            </a>
+                        </Link>
+                        <span>amin.m.intichev@gmail.com</span>
+                    </div>
+                    <div>
+                        <Link href={'https://www.instagram.com/webdevamin/'}>
+                            <a rel="noreferrer" target='_blank'>
+                                <FontAwesomeIcon icon={faInstagram} size={'xl'} />
+                            </a>
+                        </Link>
+                        <span>webdevamin</span>
+                    </div>
+                    <div>
+                        <Link href={'https://www.linkedin.com/in/amin-i-1072391b0/'}>
+                            <a rel="noreferrer" target='_blank'>
+                                <FontAwesomeIcon icon={faLinkedin} size={'xl'} />
+                            </a>
+                        </Link>
+                        <span>amin-i-1072391b0</span>
+                    </div>
+                    <div>
+                        <Link href={'https://github.com/1chbinamin'}>
+                            <a rel="noreferrer" target='_blank'>
+                                <FontAwesomeIcon icon={faGithub} size={'xl'} />
+                            </a>
+                        </Link>
+                        <span>1chbinamin</span>
+                    </div>
+                </div>
             </section>
         </footer>
     );
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,38 +7,40 @@ const Index = () => {
   return (
     <main>
       <Seo />
-      <section>
-        <div className='hero'>
-          <h1>
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter.typeString('Amin Intichev')
-                  .changeDelay(50)
-                  .start();
-              }}
-            />
-          </h1>
-          <span className='notice'>a.k.a. webdevamin</span>
-        </div>
-        <Lottie
-          loop
-          animationData={lottieJson}
-          play
-          style={{ width: '275px', height: '275px', marginLeft: 'auto', marginRight: 'auto' }}
-        />
-        <p className='highlight squada_one_font'>
-          I&apos;m a software developer from Bruges.
-        </p>
-      </section>
-      <section className='content'>
-        <h2>Who am I</h2>
-        <p className='text'>
-          I make full fledged, state-of-art websites and apps with focus on
-          your needs. Worked in various IT companies, possessing
-          advanced development knowledge. Any questions? Contact me on {' '}
-          <span className='highlight_sm'>amin.m.intichev@gmail.com</span>
-        </p>
-      </section>
+      <div className='sm_content'>
+        <section>
+          <div className='hero'>
+            <h1>
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString('Amin Intichev')
+                    .changeDelay(50)
+                    .start();
+                }}
+              />
+            </h1>
+            <span className='notice'>a.k.a. webdevamin</span>
+          </div>
+          <Lottie
+            loop
+            animationData={lottieJson}
+            play
+            style={{ width: '275px', height: '275px', marginLeft: 'auto', marginRight: 'auto' }}
+          />
+          <p className='highlight squada_one_font'>
+            I&apos;m a software developer from Bruges.
+          </p>
+        </section>
+        <section className='content'>
+          <h2>Who am I</h2>
+          <p className='text'>
+            I make full fledged, state-of-art websites and apps with focus on
+            your needs. Worked in various IT companies, possessing
+            advanced development knowledge. Any questions? Contact me on {' '}
+            <span className='highlight_sm'>amin.m.intichev@gmail.com</span>
+          </p>
+        </section>
+      </div>
     </main>
   )
 }
