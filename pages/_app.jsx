25fce8aed26488d33ca3329816e14bd75a11bfcf
1chbinamin <amin.m.intichev@gmail.com>
2022-12-07 00:30:03 +0100

    vercel analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/react';
 
 config.autoAddCss = false
 
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
