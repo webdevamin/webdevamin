3cf62d6cc6b343d18ae65097dcfdeb869df7ab19
1chbinamin <amin.m.intichev@gmail.com>
2023-01-06 15:24:06 +0100

    google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,12 +3,25 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/react';
+import Script from "next/script"
 
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <Component {...pageProps} />
       {process.env.NODE_ENV !== `development` && <Analytics />}
       <Head>
