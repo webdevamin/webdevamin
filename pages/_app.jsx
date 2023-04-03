a726731ecb01befc50d0c8445fa32a82c40804fb
--global <amin.m.intichev@gmail.com>
2023-04-03 13:32:05 +0200

    update ga4

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,16 +11,20 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} />
+        src={`https://www.googletagmanager.com/gtag/js?id=G-SMV41VQ81Z`} />
       <Script
         id='google-analytics'
-        strategy="afterInteractive">
-        {`
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-        `}
+          gtag('config', 'G-SMV41VQ81Z', {
+            page_path: window.location.pathname,
+          });
+          `,
+        }}>
       </Script>
       <Component {...pageProps} />
       {process.env.NODE_ENV !== `development` && <Analytics />}
