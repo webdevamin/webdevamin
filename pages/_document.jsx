87dc66b5cb6ba14c0aab1cc516406ac7fcbdbfbf
--global <amin.m.intichev@gmail.com>
2023-05-04 20:14:16 +0200

    facebook pixel

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,10 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Mohave:wght@300;400;500;600;700&family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+                    <noscript>
+                        <img height="1" width="1" style={{ display: 'none' }}
+                            src="https://www.facebook.com/tr?id=819616156395865&ev=PageView&noscript=1" />
+                    </noscript>
                 </Head>
                 <body>
                     <Main />
