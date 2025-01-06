730515081cb9d8d3f2a42b0692ad406f0753f940
webdevamin <amin.m.intichev@gmail.com>
2025-01-06 15:23:03 +0100

    services - webdesign minor styling DONE

--- components/Blocks/BlockCardsTwo.jsx
+++ components/Blocks/BlockCardsTwo.jsx
@@ -21,7 +21,7 @@ const BlockCardsTwo = ({ content }) => {
 
                             return (
                                 <Card key={i} className="max-w-sm bg-transparent card_two">
-                                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    <h5 className="text-xl lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                         {title}
                                     </h5>
                                     <p className="font-normal text-gray-700 dark:text-gray-400">
