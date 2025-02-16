58953301d852e1fb38d3e04fbba476c4dbaab28b
webdevamin <amin.m.intichev@gmail.com>
2025-02-16 19:12:04 +0100

    og for twitter DONE

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -31,9 +31,16 @@ const Seo = ({ seo, alternates, noIndex = false, includeCompanyName = false }) =
             <meta property="og:url" content={canonical} />
             <meta property="og:locale" content={locale} />
             <meta property="og:description" content={defaultOgDescription} />
+            <meta property="og:site_name" content="Webdevamin" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="canonical" href={canonical} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@webdevamin" />
+            <meta name="twitter:creator" content="@webdevamin" />
+            <meta name="twitter:title" content={includeCompanyName ? `${defaultOgTitle} | Webdevamin` : defaultOgTitle} />
+            <meta name="twitter:description" content={defaultOgDescription} />
+            <meta name="twitter:image" content={defaultImage} />
             {
                 alternates.map((alternate) => {
                     const { href, hreflang } = alternate;
